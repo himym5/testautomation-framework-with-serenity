2b8e214e64d0f277b4ca358883a41292f5a355b1
Julian Voss <julian.voss2u.eu>
2018-02-13 11:32:50 +0100

    Test shouldBeAbleToViewMotorProducts using screenplay pattern is now successfully running

--- src/main/java/com/cgm/life/testWithSerenitybdd/ui/NavigationBar.java
+++ src/main/java/com/cgm/life/testWithSerenitybdd/ui/NavigationBar.java
@@ -2,11 +2,14 @@ package com.cgm.life.testWithSerenitybdd.ui;
 
 import com.cgm.life.testWithSerenitybdd.model.Category;
 import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class NavigationBar {
+
     public static Target category(Category category) {
         return Target.the(category.name() + "category")
-                .locatedBy("//td[@class='cat']/a[contains(text(),'{0}')]")
+                .locatedBy("#s0-container > li:nth-child(3)")
                 .of(category.name());
     }
 }
